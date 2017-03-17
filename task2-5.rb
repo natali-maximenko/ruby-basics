8b58a4383bf2ee3cb57b9e89ff4340b5ca31312f
Natali Maximenko <natali.maximenko@gmail.com>
2017-03-17 14:58:39 +0300

    task2 fix stars rating

--- task2-5.rb
+++ task2-5.rb
@@ -1,8 +1,6 @@
-filename = ARGV[0]
+filename = ARGV[0] || 'movies.txt'
 
-if filename.nil?
-  filename = 'movies.txt'
-elsif !File.exist?(filename)
+if !File.exist?(filename)
   raise ArgumentError, 'File "'+ filename + '" not found'
 end
 
@@ -10,9 +8,10 @@ File.read(filename).split("\n").each do |line|
   info = line.split('|')
   name = info[1]
   rating = info[7]
-  stars_count = rating.split('.')[1].to_i
-  stars = '*' *stars_count
-  if name.index("Godfather")
-    puts name + '  ' + rating + ' '+stars
+  stars_count = rating.to_f * 10
+  stars = '*'  * stars_count
+  if name.index("Max") || name.index("Godfather")
+    puts name + '  ' + rating
+    puts stars
   end
 end
