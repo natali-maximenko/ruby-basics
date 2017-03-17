610f7444f73566a98d63cee11144f38caa84ddf8
Natali Maximenko <natali.maximenko@gmail.com>
2017-03-17 14:25:12 +0300

    task2 refactoring

--- task2-5.rb
+++ task2-5.rb
@@ -1,25 +1,18 @@
-fileName = ARGV[0]
+filename = ARGV[0]
 
-# if user is bad, use default file
-if fileName.nil? ||  !File.exist?(fileName)
-  puts 'No filename or file not found'
-  puts 'will be used default file: movies.txt'
-  fileName = 'movies.txt'
+if filename.nil?
+  filename = 'movies.txt'
+elsif !File.exist?(filename)
+  raise ArgumentError, 'File "'+ filename + '" not found'
 end
 
-moviesList = File.new(fileName)
-# put films info in array
-movies = moviesList.readlines
-
-#work with array
-for film in movies do
-  info = film.split('|')
+File.read(filename).split("\n").each do |line|
+  info = line.split('|')
   name = info[1]
   rating = info[7]
-  stars = '*' *rating.split('.')[1].to_i
-  if name.index("Max")
+  stars_count = rating.split('.')[1].to_i
+  stars = '*' *stars_count
+  if name.index("Godfather")
     puts name + '  ' + rating + ' '+stars
   end
 end
-
-moviesList.close
