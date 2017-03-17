04404e8361ba838a9b774fe469249508fff9f42f
Natali Maximenko <natali.maximenko@gmail.com>
2017-03-17 10:35:19 +0300

    task2 fix

--- task2-5.rb
+++ task2-5.rb
@@ -1,26 +1,21 @@
 fileName = ARGV[0]
 
 # if user is bad, use default file
-if fileName.nil? or  !File.exist?(fileName)
+if fileName.nil? ||  !File.exist?(fileName)
   puts 'No filename or file not found'
   puts 'will be used default file: movies.txt'
   fileName = 'movies.txt'
 end
 
 moviesList = File.new(fileName)
-movies = []
-i = 0
 # put films info in array
-begin
-  info = moviesList.readline.split('|')
-  movies[i] = info
-  i += 1
-end until moviesList.eof?
+movies = moviesList.readlines
 
 #work with array
 for film in movies do
-  name = film[1]
-  rating = film[7]
+  info = film.split('|')
+  name = info[1]
+  rating = info[7]
   stars = '*' *rating.split('.')[1].to_i
   if name.index("Max")
     puts name + '  ' + rating + ' '+stars
