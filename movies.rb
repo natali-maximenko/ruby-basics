754e012ad78544f9ecc87e868c4764676e15c765
Natali Maximenko <natali.maximenko@gmail.com>
2017-03-21 16:39:33 +0300

    task3 fix

--- movies.rb
+++ movies.rb
@@ -1,7 +1,5 @@
-require 'date'
-
 def print_movie_info(film)
-  puts film[:title] + '(' + film[:release_date] +'; ' + film[:genre] +'; ' + film[:country] +'; ' + film[:rating] +') - ' + film[:length]
+  puts "#{film[:title]} ( #{film[:release_date]}; #{film[:genre]}; #{film[:country]}; #{film[:rating]}) - #{film[:length]}"
 end
 
 filename = ARGV[0] || 'movies.txt'
@@ -10,57 +8,29 @@ if !File.exist?(filename)
   raise ArgumentError, 'File "'+ filename + '" not found'
 end
 
-movies_list = []
-
-File.read(filename).split("\n").each do |line|
-  info = line.split('|')
-  film = {
-      link: info[0],
-      title: info[1],
-      year: info[2],
-      country: info[3],
-      release_date: info[4],
-      genre: info[5],
-      length: info[6],
-      rating: info[7],
-      director: info[8],
-      actors: info[9]
-  }
-  movies_list.push(film)
-end
-
+info = [:link, :title, :year, :country, :release_date, :genre, :length, :rating, :director, :actors]
+movies_list = File.open(filename).readlines.map! {|l| info.zip(l.split('|')).to_h }
 
 # sort by length desc
-sorted_by_size = movies_list.sort { |x, y| y.fetch(:length).to_i <=> x.fetch(:length).to_i}
+sorted_by_size = movies_list.sort_by { |x| x[:length].to_i}.reverse
 puts '5 movies sorted by length desc'
-sorted_by_size[0..4].each { |movie| print_movie_info(movie) }
+sorted_by_size.first(5).each { |movie| print_movie_info(movie) }
 puts
 
-# find comedies
-comedies = movies_list.select { |e| e[:genre].split(',').include?('Comedy') }
-# sort by release date asc
-sorted_by_release = comedies.sort { |x, y|
-  x_year = x[:release_date].split('-').first.to_i
-  y_year = y[:release_date].split('-').first.to_i
-  x_year <=> y_year
-  #a = Date::strptime(x[:release_date], '%Y-%m-%d')
-  #b = Date::strptime(y[:release_date], '%Y-%m-%d')
-  #a <=> b
-}
+# comedies sort by release date asc
+sorted_by_release = movies_list.select { |e| e[:genre].split(',').include?('Comedy') }
+  .sort_by { |x| x[:release_date] }
 puts '10 comedies sorted release date asc'
-sorted_by_release[0..9].each { |movie| print_movie_info(movie) }
+sorted_by_release.first(10).each { |movie| print_movie_info(movie) }
 puts
 
-directors = []
-movies_list.each { |e|
-  if !directors.include?(e.fetch(:director))
-    directors.push(e.fetch(:director))
-  end
-}
-sorted_by_firstname = directors.sort_by { |x| x.split(' ').last }
+directors = movies_list.map {|x| x[:director] }
+  .uniq
+  .sort_by { |x| x.split(' ').last }
 puts 'Unique directors array sorted by firstname asc'
-print sorted_by_firstname
+print directors
 puts
 
-not_usa_production = movies_list.select { |e| e[:country] != 'USA' }
-puts 'Count film not USA production = ' + not_usa_production.length.to_s
+#not_usa_production = movies_list.reject { |e| e[:country] == 'USA' }
+not_usa_production = movies_list.count { |x| x[:country] != 'USA' }
+puts "Count film not USA production = #{not_usa_production}"
