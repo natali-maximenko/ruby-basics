60c732fec0be258e79055ea228b21a7c402118d6
Natali Maximenko <natali.maximenko@gmail.com>
2017-04-06 12:21:36 +0300

    task6 movie_collection_spec fix

--- movie_collection_spec.rb
+++ movie_collection_spec.rb
@@ -2,20 +2,34 @@ require 'rspec'
 require './movie_collection.rb'
 
 describe Netflix do
-  before do
-    @netflix = Netflix.new('movies.txt')
-  end
 
   it '#pay' do
-    @netflix.pay(25)
-    expect(@netflix.account).to eq(25)
+    netflix = Netflix.new('movies.txt')
+    netflix.pay(25)
+    expect(netflix.account).to eq(25)
   end
 
-  it 'when no money on account' do
-    @netflix.show(genre: 'Comedy', period: :classic)
-    it 'raise exception' do
-      expect().to raise_error ArgumentError
+  describe '#show' do
+
+    context 'when no money on account' do
+      netflix = Netflix.new('movies.txt')
+      it 'raise exception' do
+        expect { netflix.show(genre: 'Comedy', period: :classic) }.to raise_error ArgumentError
+      end
     end
-  end
 
+    context 'when need more money' do
+      netflix = Netflix.new('movies.txt')
+      netflix.pay(7)
+      it 'raise exception with need no money' do
+        expect { netflix.show(genre: 'Comedy', period: :classic) }.to raise_error ArgumentError
+      end
+    end
+
+    context 'when have enough money' do
+      it 'shows all selected films'
+    end
+
+
+  end
 end
